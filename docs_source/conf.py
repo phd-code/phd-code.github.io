6eb96da276273ccf805cd102f031e7e9994fd20b
Sahil Hegde <sahilhegde@Sahils-MacBook-Pro.local>
2021-06-29 13:11:56 -0700

    Docs updated and rebuilt

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -14,7 +14,10 @@ import os
 import sys
 import mock
 sys.path.insert(0, os.path.abspath('../phd/'))
-#sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../phd/containers/'))
+sys.path.insert(0, os.path.abspath('../phd/domain/'))
+
 #sys.path.insert(0, os.path.abspath('./'))
 
 import furo
