95197e1ae75d9a649c63e2ea647615c1c7055d33
Sahil Hegde <sahilhegde@Sahils-MacBook-Pro.local>
2021-06-29 12:29:06 -0700

    Docs updated and rebuilt

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -15,7 +15,7 @@ import sys
 import mock
 #sys.path.insert(0, os.path.abspath('../phd/'))
 sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('./'))
+#sys.path.insert(0, os.path.abspath('./'))
 
 import furo
 import insipid_sphinx_theme
